eef63ef0e5d0b7272834f85bb5201a7201edbdef
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-21 20:06:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,43 @@ cluster_topics = load_cluster_topics()
 tsne_2d_vectors = load_tsne_2d_vectors()
 df_cluster = load_tsne_posts_vectors_clusters()
 
+
+# Chart
+alt.data_transformers.disable_max_rows()
+
+colors = [
+  '#fff100', # yellow
+  '#ff8c00', # orange
+  '#e81123', # red
+  '#ec008c', # magenta
+  '#68217a', # purple
+  '#00188f', # blue
+  '#00bcf2', # cyan
+  '#00b294', # teal
+  '#009e49', # green
+  '#bad80a' # lime
+]
+
+def my_theme():
+  return {
+    'config': {
+      'view': {'continuousHeight': 400, 'continuousWidth': 400},  # from the default theme
+      'range': {'category': colors}
+    }
+  }
+alt.themes.register('my_theme', my_theme)
+alt.themes.enable('my_theme')
+
+tsne_plot = alt.Chart(df_cluster).mark_circle(size=60).encode(
+                x=alt.X('x:Q', axis=alt.Axis(title='Dimension 1', titlePadding=12, titleFontSize=16, titleFontWeight=900)),
+                y=alt.Y('y:Q', axis=alt.Axis(title='Dimension 2', titlePadding=12, titleFontSize=16, titleFontWeight=900)),
+                # x='x:Q',
+                # y='y:Q',
+                color='cluster:N',
+                opacity=alt.value(0.3),
+                tooltip=['title', 'cluster']
+            )
+
+# tsne_plot
+
+
