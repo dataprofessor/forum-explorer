5b0aad431420892592aa5206fe0731d73d5a86ba
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-21 19:57:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,19 +7,19 @@ st.title('🎈 Streamlit Forum Explorer')
 
 @st.cache_data
 def load_df():
-  return pd.read_csv('streamlit_forum_16Jan2024.csv')
+  return pd.read_csv('data/streamlit_forum_16Jan2024.csv')
 
 @st.cache_data
 def load_embeddings():
-  return torch.load(f'corpus_embeddings_16Jan2024.pt')
+  return torch.load('data/corpus_embeddings_16Jan2024.pt')
 
 @st.cache_data
 def load_cluster_topics():
-  return pd.read_csv('cluster_topics.csv', header=False)
+  return pd.read_csv('data/cluster_topics.csv', header=False)
 
 @st.cache_data
 def load_tsne_2d_vectors():
-  return np.load('tsne_2d_vectors.npy')
+  return np.load('data/tsne_2d_vectors.npy')
 
 df = load_df()
 corpus_embeddings = load_embeddings()
