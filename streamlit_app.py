333ea2062da5cc6dea8a8d455a9517da8e25165e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-21 19:33:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import torch
 
 st.title('🎈 Streamlit Forum Explorer')
 
-st.write('Hello world!')
+@st.cache_data
+def load_df():
+  return pd.read_csv('streamlit_forum_16Jan2024.csv')
+
+@st.cache_data
+def load_embeddings():
+  return torch.load(f'corpus_embeddings_16Jan2024.pt')
+
+df = load_df()
+corpus_embeddings = load_embeddings()
