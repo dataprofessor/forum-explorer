c9071975781e90adc707222e22f791e58f274f55
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-21 19:39:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,15 @@ def load_df():
 def load_embeddings():
   return torch.load(f'corpus_embeddings_16Jan2024.pt')
 
+@st.cache_data
+def load_cluster_topics():
+  return pd.read_csv('cluster_topics.csv', header=False)
+
+@st.cache_data
+def load_tsne_2d_vectors():
+  return np.load('tsne_2d_vectors.npy')
+
 df = load_df()
 corpus_embeddings = load_embeddings()
+cluster_topics = load_cluster_topics()
+tsne_2d_vectors = load_tsne_2d_vectors()
