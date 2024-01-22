130c04551a233258dba06912899af5a5f0898f39
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-22 10:52:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,6 +78,7 @@ st.altair_chart(tsne_plot, use_container_width=True)
 # Generate embeddings for query
 embedder = SentenceTransformer('all-MiniLM-L6-v2')
 query_embedding = embedder.encode(input_query, convert_to_tensor=True)
+corpus = list(df.title)
 
 # Find the highest 5 scores
 top_k = min(5, len(corpus_embeddings))
@@ -87,7 +88,5 @@ top_results = torch.topk(cos_scores, k=top_k)
 if input_query is not None:
   st.write(input_query)
 
-  corpus = list(df.title)
-  
   for score, idx in zip(top_results[0], top_results[1]):
     st.write(corpus[idx], "(Score: {:.4f})".format(score))
