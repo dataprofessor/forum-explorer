024fec80e7466bf969c79cd55f235192c8e617b2
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-22 11:16:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ if input_query is not None:
 
   for score, idx in zip(top_results[0], top_results[1]):
     st.write(corpus[idx], "(Score: {:.4f})".format(score))
-    st.write(df.slug[idx.item()])
+    st.write(df.slug[idx])
     # https://discuss.streamlit.io/t/{df.slug[idx]}/{df.id[idx]}
 
 ##########
