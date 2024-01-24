af06eae7bb0d82435abb08469e3db4b7f4700fd4
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-24 13:52:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,13 +96,8 @@ if input_query != '':
     post_link = f"https://discuss.streamlit.io/t/{df.slug[idx.item()]}/{df.id[idx.item()]}"
 
     if score >= score_threshold:
-      if df.has_accepted_answer[idx.item()]=='True':
-        solution = '✅'
-        st.write(f"- [{corpus[idx]}]({post_link})", "|", "Score: `{:.3f}`".format(score), "|", f"Solution: `{solution}`")
-      if df.has_accepted_answer[idx.item()]=='False':
-        solution = '❌'
-        st.write(f"- [{corpus[idx]}]({post_link})", "|", "Score: `{:.3f}`".format(score), "|", f"Solution: `{solution}`")
-      #st.write(f"- [{corpus[idx]}]({post_link})", "|", "Score: `{:.3f}`".format(score), "|", f"Solution: `{solution}`")
+      solution = df.has_accepted_answer[idx.item()]
+      st.write(f"- [{corpus[idx]}]({post_link})", "|", "Score: `{:.3f}`".format(score), "|", f"Solution: `{solution}`")
 
   # Apply tSNE model on query embeddings
   tsne_query_embedding = tsne_corpus_embeddings.transform(query_embedding.unsqueeze(0))
