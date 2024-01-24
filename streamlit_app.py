221bef21831774265bc7ae2fa27d492e373f03df
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-24 22:49:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,17 @@ import requests
 from sentence_transformers import SentenceTransformer, util
 import torch
 
+# Page title
+st.set_page_config(page_title='Streamlit Forum Explorer', page_icon='🎈')
 st.title('🎈 Streamlit Forum Explorer')
 
+# About the app
+with st.expander('About this app'):
+  st.markdown('**What can this app do?**')
+  st.info('This app shows collates data from the Streamlit forum and suggests related forum posts to user's input query.')
+  st.markdown('**How to use the app?**')
+  st.warning('To use the app, firstly enter an input query. The app will perform processing and generate a list of relevant forum posts based on K-means nearest neighbor search. Additionally, ')
+  
 # Load data
 @st.cache_data
 def load_df():
